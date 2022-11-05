520c3ff5f672c3cee4c28abea5dfb938e52e51d0
Carlos Daniel Sanjuan <carlosdsanjuan@outlook.com>
2022-11-05 12:22:05 -0500

    Creating first routes

--- index.js
+++ index.js
@@ -9,6 +9,25 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/new-route', (req, res) => {
+  res.send('Hello, i am a new route!');
+});
+
+app.get('/products', (req, res) => {
+  res.json({
+    products: [
+      {
+        id: 1,
+        name: 'Product 1',
+      },
+      {
+        id: 2,
+        name: 'Product 2',
+      }
+    ],
+  });
+});
+
 app.listen(config.port, () => {
   console.log(`Server is running on localhost:${config.port}`);
 });
