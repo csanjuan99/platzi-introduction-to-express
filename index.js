746f2df73328250ef06cfa4d2caa13aa638dac10
Carlos Daniel Sanjuan <carlosdsanjuan@outlook.com>
2022-11-05 12:13:52 -0500

    Install express.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const express = require('express');
+
+const app = express();
+const config = {
+  port: 3000,
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(config.port, () => {
+  console.log(`Server is running on localhost:${config.port}`);
+});
+
