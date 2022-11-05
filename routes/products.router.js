04b953df86fd06306c9fb1347e0226e56aa747d4
Carlos Daniel Sanjuan <carlosdsanjuan@outlook.com>
2022-11-05 15:06:28 -0500

    Implementing async await on methods & handling erros with try catch

--- routes/products.router.js
+++ routes/products.router.js
@@ -4,36 +4,42 @@ const ProductsServices = require('../services/products.service');
 
 const service = new ProductsServices();
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const {body} = req;
-  const product = service.create(body);
+  const product = await service.create(body);
   res.status(201).json(product);
 });
 
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.status(200).json({
     data: products
   });
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const {id} = req.params;
-  const product = service.findOne(id);
+  const product = await service.findOne(id);
   res.status(200).json(product);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const {id} = req.params;
   const {body} = req;
-  const product = service.update(id, body);
+  const product = await service.update(id, body);
   res.status(200).json(product);
 });
 
-router.delete('/:id', (req, res) => {
-  const {id} = req.params;
-  const product = service.delete(id);
-  res.status(200).json(product);
+router.delete('/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const product = await service.delete(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(404).json({
+      message: error.message
+    });
+  }
 });
 
 module.exports = router;
