44e17f56da65dc68a29b511bdc7679ca50c2f232
Carlos Daniel Sanjuan <carlosdsanjuan@outlook.com>
2022-11-07 09:58:15 -0500

    Implementing middlewares

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const api = require('./routes');
+const {logErrors, errorHandler} = require('./middleware/error.handler');
 
 const config = {
   port: 3000,
@@ -8,6 +9,8 @@ const config = {
 app.use(express.json());
 
 api(app);
+app.use(logErrors);
+app.use(errorHandler);
 app.listen(config.port, () => {
   console.log(`Server is running on localhost:${config.port}`);
 });
---
+++ middleware/error.handler.js
@@ -0,0 +1,20 @@
+const {response} = require("express");
+
+function logErrors(err, req, res, next) {
+  console.log('logErrors');
+  console.log(err);
+  next(err);
+}
+
+function errorHandler(err, req, res, next) {
+  console.log('errorHandler');
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+}
+
+module.exports = {
+  errorHandler,
+  logErrors,
+};
